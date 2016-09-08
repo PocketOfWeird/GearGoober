b861527e7808c76ac699f8aaa04a7ca95a88cf0e
Pocket of Weird <hartzlernathan@gmail.com>
2016-09-08 15:06:54 -0500

    removed unnecissary Gear.data functions

--- public/app/views/equipment.js
+++ public/app/views/equipment.js
@@ -3,13 +3,13 @@
     $('#equipmentTabs').render(Gear.user, Gear.directives);
 
     // show inner search bar if on mobile
-    if (Gear.isMobile) {
+    if (Gear.isMobile()) {
         $('#equipment-mobile-search').removeClass("hidden");
     }
 
 
     // get all categories from the db
-    Gear.data.getCategory({}).then(function(categories){
+    api.getData('/api/equipment/categories/', {}).then(function(categories){
         // Switch plugin
         aja().url('assets/js/bootstrap-checkbox-radio-switch-tags.js').type('script').go();
         $('#category-list').render(categories, Gear.directives);
