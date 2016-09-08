044db04fe400ac71d328673c847b02a4de6f5d6e
Pocket of Weird <hartzlernathan@gmail.com>
2016-09-08 10:57:36 -0500

    added switches to equipment categories

--- public/app/views/equipment.js
+++ public/app/views/equipment.js
@@ -2,6 +2,8 @@
     $('#equipmentTabs').render(Gear.user, Gear.directives);
     
     Gear.data.getCategory({}).then(function(categories){
+        // Switch plugin
+        aja().url('assets/js/bootstrap-checkbox-radio-switch-tags.js').type('script').go();
         $('#category-list').render(categories, Gear.directives);
 
     }).catch(function(error){
