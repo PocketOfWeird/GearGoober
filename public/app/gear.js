bcde64b0fa5f32011f4f0c78d728231dd2ca6260
Pocket of Weird <hartzlernathan@gmail.com>
2016-09-13 16:09:56 -0500

    initial viewloader implementation

--- public/app/gear.js
+++ public/app/gear.js
@@ -27,6 +27,7 @@
  * @requires immutable.js - Immutable data structures
  * @requires rsvp.js - Promise library
  * @requires grapnel.js - Client-side router
+ * @requires aja.js - AJAX library
  * @requires transparency.js - Data to DOM binding
  */
 
@@ -44,38 +45,27 @@
  * Application routes for the grapnel.js client-side router
  * 
  * @readonly
- * @const {ImmutableMap}
+ * @const {Map}
  * 
  * @see https://github.com/baseprime/grapnel#declaring-multiple-routes 
  */
-var routes = Immutable.Map({
-    '/':'routeHome'
-});
+const routes = {
+    '/' : routeHome,
+    '/:view/:subview' : routeView
+};
 /**
  * Rules (Directives) for the transparency.js DOM rendering engine 
  * 
  * @readonly
- * @const {ImmutableMap}
+ * @const {Map}
  * 
  * @see https://github.com/leonidas/transparency/blob/master/README.md#directives
  */
-var rulesForRender = Immutable.Map({
+const rulesForRender = {
     image: { src: function() { return this.imageUrl; } },
     subCategory: { subCategoryName: { text: function() { return this.value } } },
     inKit: { class: function() { this.inKit ? this.class : 'hidden'; } }
-});
-/**
- * A collection of html elements that each represent an inividual view
- * 
- * @readonly
- * @const {NestedImmutableMap}
- */
-var views = Immutable.fromJS({
-    home: {
-        html: ``
-    }
-});
-
+};
 
 /**============================================================================
  * ============================================================================
@@ -86,35 +76,60 @@ var views = Immutable.fromJS({
  */
 
 /**
- * Handles the logic for the home route
+ * @func load
+ * @desc Loads an HTML View into index.html's view portal. 
+ * Uses the aja.js plugin {@link http://krampstudio.com/aja.js/}
+ * 
+ * @param {String} view - the name of the view
+ * @param {String} subview - the name of the subview
+ * @param {String} viewportal - the element selector for the view portal
+ * @returns {Promise}
+ */
+function load(view, subview, viewportal) {
+    const url = 'views/' + view + '/' + subview +'.html';
+    
+    var promise = new RSVP.Promise(function(resolve, reject) {
+        aja().url(url).into(viewportal)
+        .on('success', function(response){ resolve(response) })
+        .on('error', function(response){ reject(response) }).go();
+    });
+    return promise;
+}
+/**
+ * @func render
+ * @desc Renders data into the view
+ */
+/**
+ * @func routeHome
+ * @desc Handles the logic for the home route
  * 
  * @param {HTTP Request Object} req
- * @returns {ViewObject}
+ * @returns {Boolean}
  */
 function routeHome(req) {
-
+    console.log('ET Phone Home');
 }
+/**
+ * @func routeView
+ * @desc Handles the logic for a view route.
+ * 
+ * @param {HTTP Request Object} req
+ * @returns {Void}
+ */
+function routeView(req) {
 
-/*
-var gear = Immutable.fromJS({
-    render: function(renderDirectives, element, data) { 
-        return new RSVP.Promise(function(resolve, reject) { 
-            try {
-                Transparency.render(element, data, renderDirectives);
-                resolve('success');
-            } catch (error) {
-                reject(error);
-            }
-        }); 
-    },
-    router: function(routeDirectives) {
-        var router = new Grapnel();
-        router.get('/', routeDirectives.get('home'));    
-    }
-});
+    const view = req.params.view;
+    const subview = req.params.subview;
+    const viewportal = '#view-portal';
 
-gear.get('router')(directives.get('routeDirectives'));
-*/
+    // TODO: show loader gif
+    load(view, subview, viewportal).then(function() {
+        // TODO: hide loader gif
+    }).catch(function(error) {
+        // TODO: handle error
+        console.log(error);
+    });
+}
 
 /**============================================================================
  * ============================================================================
@@ -124,12 +139,18 @@ gear.get('router')(directives.get('routeDirectives'));
  * ============================================================================
  */
 /**
- *  Program execution starts here
- * 
- * @function gear
+ * @function gear - Program main function 
  * @param {Boolean} starting
+ * @param {Map} [routes]
  * @returns {Boolean}
  */
-function gear(starting) {
-
+function gear(starting, routes) { 
+    if (starting) {
+        var router = Grapnel.listen(routes);
+        return router ? true : false;
+    }
+    return false;
 }
+
+//Program execution starts here
+console.log(gear(true, routes) ? 'GearGoober is started' : 'GearGoober is not started');
