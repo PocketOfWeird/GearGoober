3a3775cda88cc11e973974b72c1a20dde0d50ccd
Pocket of Weird <hartzlernathan@gmail.com>
2016-09-14 09:49:19 -0500

    refactored cookies setup

--- public/app/core/cookie-setup.js
+++
@@ -1,6 +0,0 @@
-Cookies.defaults = {
-    path: '/app/',
-    domain: 'localhost', // TODO: switch to host domain
-    expires: 86400, // expires in 24 hours
-    //TODO: secure: true // requires https
-}
