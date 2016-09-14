931f6b983b0e8948e07536f817ebb67d612ea9a9
Pocket of Weird <hartzlernathan@gmail.com>
2016-09-14 09:40:32 -0500

    wrapped application logic in an anonymous IIFE

--- public/app/gear.js
+++ public/app/gear.js
@@ -32,167 +32,173 @@
  */
 
 "use strict"; // sets the debugger to be a big meany if nesiccary
-
-/**============================================================================
- * ============================================================================
- * %% ++ DATA LIST ++ %%
- * An alphabetical list of data used by the main application
- * ============================================================================
- * ============================================================================
- */
-
 /**
- * Application routes for the grapnel.js client-side router
- * 
- * @readonly
- * @const {Map}
- * 
- * @see https://github.com/baseprime/grapnel#declaring-multiple-routes 
+ * Everything is wrapped in an immediatelly-invoked anonymous function
+ * to prevent littering the global namespace
+ * See {@link https://en.wikipedia.org/wiki/Immediately-invoked_function_expression}
  */
-const routes = {
-    '/' : routeHome,
-    '/:view/:subview/:query' : routeView,
-    '/:view/:subview' : routeView
-};
-/**
- * Rules (Directives) for the transparency.js DOM rendering engine 
- * 
- * @readonly
- * @const {Map}
- * 
- * @see https://github.com/leonidas/transparency/blob/master/README.md#directives
- */
-const rulesForRender = {
-    image: { src: function() { return this.imageUrl; } },
-    subCategory: { subCategoryName: { text: function() { return this.value } } },
-    inKit: { class: function() { this.inKit ? this.class : 'hidden'; } }
-};
-
-/**============================================================================
- * ============================================================================
- * %% ++ FUNCTION LIST ++ %%
- * An alphabetical list of data used by the main application
- * ============================================================================
- * ============================================================================
- */
-
-/**
- * @func getFromApi
- * @desc Makes get requests to the api backend and returns the data.
- * Call the {@link func:getFromCookie} function for auth data
- * Uses the aja.js plugin {@link http://krampstudio.com/aja.js/}
- * 
- * @param {String} url
- * @param {JSON} query
- * @calls func:getFromCookie
- * @returns {Promise|ImmutableList}
- */
-/**
- * @func getFromCookie
- * @desc Makes get requests to the local cookies and returns the data.
- * Uses the cookies.js plugin {@link https://github.com/ScottHamper/Cookies}
- * 
- * @param {String} key
- * @returns {Promise|ImmutableMap}
- */
-/**
- * @func loadHtml
- * @desc Loads an HTML View into index.html's view portal. 
- * Uses the aja.js plugin {@link http://krampstudio.com/aja.js/}
- * 
- * @param {String} view - the name of the view
- * @param {String} subview - the name of the subview
- * @param {String} viewportal - the element selector for the view portal
- * @returns {Promise}
- */
-function loadHtml(view, subview, viewportal) {
-    const url = 'views/' + view + '/' + subview +'.html';
-    
-    var promise = new RSVP.Promise(function(resolve, reject) {
-        aja().url(url).into(viewportal)
-        .on('success', function(response){ resolve(response) })
-        .on('error', function(response){ reject(response) }).go();
-    });
-    return promise;
-}
-/**
- * @func render
- * @desc Renders data into the view
- */
-/**
- * @func routeHome
- * @desc Handles the logic for the home route
- * 
- * @param {HTTP Request Object} req
- * @returns {Boolean}
- */
-function routeHome(req) {
-    console.log('ET Phone Home');
-}
-/**
- * @func routeView
- * @desc Handles the logic for a view route.
- * 
- * @param {HTTP Request Object} req
- * @returns {Void}
- */
-function routeView(req) {
-
-    const view = req.params.view;
-    const subview = req.params.subview;
-    const query = req.params.query;
-    const viewportal = '#view-portal';
+(function() {
+    /**============================================================================
+     * ============================================================================
+     * %% ++ DATA LIST ++ %%
+     * An alphabetical list of data used by the main application
+     * ============================================================================
+     * ============================================================================
+     */
 
     /**
-     * Application views
-     * @see http://facebook.github.io/immutable-js/docs/#/fromJS
+     * Application routes for the grapnel.js client-side router
+     * 
+     * @readonly
+     * @const {Map}
+     * 
+     * @see https://github.com/baseprime/grapnel#declaring-multiple-routes 
      */
-    const viewRegistry = Immutable.fromJS({
-        equipment : {
-            add: { data: false, api: '', cookie: '' },
-            search: { data: true, api: 'equipment', cookie: ''  }
-        }
-    });
+    const routes = {
+        '/' : routeHome,
+        '/:view/:subview/:query' : routeView,
+        '/:view/:subview' : routeView
+    };
+    /**
+     * Rules (Directives) for the transparency.js DOM rendering engine 
+     * 
+     * @readonly
+     * @const {Map}
+     * 
+     * @see https://github.com/leonidas/transparency/blob/master/README.md#directives
+     */
+    const rulesForRender = {
+        image: { src: function() { return this.imageUrl; } },
+        subCategory: { subCategoryName: { text: function() { return this.value } } },
+        inKit: { class: function() { this.inKit ? this.class : 'hidden'; } }
+    };
 
-    // Check if the view is not registered
-    if (!viewRegistry.hasIn([view, subview])) console.log('404! ' + view + ' not found'); // TODO: return 404 view
+    /**============================================================================
+     * ============================================================================
+     * %% ++ FUNCTION LIST ++ %%
+     * An alphabetical list of data used by the main application
+     * ============================================================================
+     * ============================================================================
+     */
 
-    // TODO: show loader gif
-    loadHtml(view, subview, viewportal)
-    .then( function() {
-        // Check if view needs data
-        if (viewRegistry.hasIn([view, subview, 'data'])) {
-            // TODO: check for api or cookie via switch
-        } else {
-            // TODO: hide loader gif
-        }
-    } )
-    .catch(function(error) {
-        // TODO: handle error
-        console.log(error);
-    });
-}
-
-/**============================================================================
- * ============================================================================
- * %% ++ MAIN ++ %%
- * The main application execution entry point
- * ============================================================================
- * ============================================================================
- */
-/**
- * @function gear - Program main function 
- * @param {Boolean} starting
- * @param {Map} [routes]
- * @returns {Boolean}
- */
-function gear(starting, routes) { 
-    if (starting) {
-        var router = Grapnel.listen(routes);
-        return router ? true : false;
+    /**
+     * @func getFromApi
+     * @desc Makes get requests to the api backend and returns the data.
+     * Call the {@link func:getFromCookie} function for auth data
+     * Uses the aja.js plugin {@link http://krampstudio.com/aja.js/}
+     * 
+     * @param {String} url
+     * @param {JSON} query
+     * @calls func:getFromCookie
+     * @returns {Promise|ImmutableList}
+     */
+    /**
+     * @func getFromCookie
+     * @desc Makes get requests to the local cookies and returns the data.
+     * Uses the cookies.js plugin {@link https://github.com/ScottHamper/Cookies}
+     * 
+     * @param {String} key
+     * @returns {Promise|ImmutableMap}
+     */
+    /**
+     * @func loadHtml
+     * @desc Loads an HTML View into index.html's view portal. 
+     * Uses the aja.js plugin {@link http://krampstudio.com/aja.js/}
+     * 
+     * @param {String} view - the name of the view
+     * @param {String} subview - the name of the subview
+     * @param {String} viewportal - the element selector for the view portal
+     * @returns {Promise}
+     */
+    function loadHtml(view, subview, viewportal) {
+        const url = 'views/' + view + '/' + subview +'.html';
+        
+        var promise = new RSVP.Promise(function(resolve, reject) {
+            aja().url(url).into(viewportal)
+            .on('success', function(response){ resolve(response) })
+            .on('error', function(response){ reject(response) }).go();
+        });
+        return promise;
     }
-    return false;
-}
+    /**
+     * @func render
+     * @desc Renders data into the view
+     */
+    /**
+     * @func routeHome
+     * @desc Handles the logic for the home route
+     * 
+     * @param {HTTP Request Object} req
+     * @returns {Boolean}
+     */
+    function routeHome(req) {
+        console.log('ET Phone Home');
+    }
+    /**
+     * @func routeView
+     * @desc Handles the logic for a view route.
+     * 
+     * @param {HTTP Request Object} req
+     * @returns {Void}
+     */
+    function routeView(req) {
 
-//Program execution starts here
-console.log(gear(true, routes) ? 'GearGoober is started' : 'GearGoober is not started');
+        const view = req.params.view;
+        const subview = req.params.subview;
+        const query = req.params.query;
+        const viewportal = '#view-portal';
+
+        /**
+         * Application views
+         * @see http://facebook.github.io/immutable-js/docs/#/fromJS
+         */
+        const viewRegistry = Immutable.fromJS({
+            equipment : {
+                add: { data: false, api: '', cookie: '' },
+                search: { data: true, api: 'equipment', cookie: ''  }
+            }
+        });
+
+        // Check if the view is not registered
+        if (!viewRegistry.hasIn([view, subview])) console.log('404! ' + view + ' not found'); // TODO: return 404 view
+
+        // TODO: show loader gif
+        loadHtml(view, subview, viewportal)
+        .then( function() {
+            // Check if view needs data
+            if (viewRegistry.hasIn([view, subview, 'data'])) {
+                // TODO: check for api or cookie via switch
+            } else {
+                // TODO: hide loader gif
+            }
+        } )
+        .catch(function(error) {
+            // TODO: handle error
+            console.log(error);
+        });
+    }
+
+    /**============================================================================
+     * ============================================================================
+     * %% ++ MAIN ++ %%
+     * The main application execution entry point
+     * ============================================================================
+     * ============================================================================
+     */
+    /**
+     * @function gear - Program main function 
+     * @param {Boolean} starting
+     * @param {Map} [routes]
+     * @returns {Boolean}
+     */
+    function gear(starting, routes) { 
+        if (starting) {
+            var router = Grapnel.listen(routes);
+            return router ? true : false;
+        }
+        return false;
+    }
+
+    //Program execution starts here
+    console.log(gear(true, routes) ? 'GearGoober is started' : 'GearGoober is not started');
+})();
