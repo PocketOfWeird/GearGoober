7e282b25cf2b17518cbd3022c620bca4378a6770
Pocket of Weird <hartzlernathan@gmail.com>
2016-09-16 12:17:53 -0500

    initial startup in react

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * @file index.js
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello, world again!</h1>,
+  document.getElementById('main')
+);
