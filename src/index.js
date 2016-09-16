1948632a950096275e3a2faeb7a5647dddf52511
Pocket of Weird <hartzlernathan@gmail.com>
2016-09-16 16:25:18 -0500

    egghead.io redux basics tut p12

--- src/index.js
+++ src/index.js
@@ -5,17 +5,36 @@ import { createStore } from 'redux';
 import expect from 'expect';
 var deepFreeze = require('deep-freeze');
 
+
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+          id: action.id,
+          text: action.text,
+          completed: false
+        };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+          return state;
+        }
+        return Object.assign({}, state, 
+            { completed: !state.completed }
+        );
+    default:
+      return state;
+  }
+};
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
+        todo(undefined, action)
       ]
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
     default:
       return state;
   }
@@ -44,5 +63,44 @@ const testAddTodo = () => {
   ).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false
+    }
+  ];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+};
+
 testAddTodo();
+testToggleTodo();
 console.log('All tests passed.');
