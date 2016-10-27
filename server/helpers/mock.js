cdf01044650c36a2ff87a44ad2900d59aefa3fd7
Pocket of Weird <hartzlernathan@gmail.com>
2016-10-27 16:11:31 -0500

    Added permissions to user mockup

--- server/helpers/mock.js
+++ server/helpers/mock.js
@@ -7,7 +7,12 @@ const userPassword = hash('boogerface')
 
 const mockSnapshop = {
   tennants: { [tennantId]: { name: 'Demo University' }},
-  users: { [userId]: { tennant: tennantId, email: 'bob@demo.edu', password: userPassword } },
+  users: {
+    [userId]: {
+      tennant: tennantId, email: 'bob@demo.edu', password: userPassword,
+      permissions: { admin: true, manager: true, labworker: true } 
+    }
+  },
   lookup: { users: { 'bob@demo.edu': userId } }
 }
 
