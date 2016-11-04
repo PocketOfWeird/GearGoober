6118141c5d01fe67e716206e90a8f7f74f526d9d
Pocket of Weird <hartzlernathan@gmail.com>
2016-11-04 15:54:50 -0500

    Initial firebase auth setup

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,39 @@
 import Firebase from 'firebase'
 import config from './config'
 
+const firebase = Firebase.initializeApp(config)
+export default firebase
 
-export const firebase = Firebase.initializeApp(config)
-
+// ============================================================================
+// AUTHENTICATION
+// ============================================================================
 export const authenticated = () => firebase.currentUser ? true : false
 
-export const loginWith = provider => {
-  horizon.authEndpoint(provider).subscribe(endpoint => {
-    window.location.replace(endpoint)
-  })
+const providers = {
+  'github': new Firebase.auth.GithubAuthProvider()
 }
+
+export const loginWith = provider => {
+  firebase.auth().signInWithRedirect(providers[provider])
+}
+
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Provider Access Token. You can use it to access the Provider's API.
+    var token = result.credential.accessToken
+    console.log('token', token)
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user
+  console.log('user', user)
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code
+  var errorMessage = error.message
+  // The email of the user's account used.
+  var email = error.email
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential
+  // ...
+})
