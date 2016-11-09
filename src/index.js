e4c694715935818191ad2bbe62a524a4749aa5f7
Pocket of Weird <hartzlernathan@gmail.com>
2016-11-09 15:48:33 -0600

    Refactored root component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import Root from './containers'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import configureStore from './store/configureStore'
+import App from './containers/app'
+
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin()
 
+// Create the redux store
+let store = configureStore()
+store.dispatch({ type: 'INITIALIZE'})
+
+// Build the root React Component
 ReactDOM.render(
-  <Root />,
+  <Provider store={store}>
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
+  </Provider>,
   document.getElementById('root')
 )
